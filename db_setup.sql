9c028aca72be7823f2d6b00506e792842ae873c6
dmalyuta <danylo.malyuta@gmail.com>
2017-04-01 10:18:49 -0700

    database setup file

---
+++ db_setup.sql
@@ -0,0 +1,20 @@
+# Setup the database of questions
+CREATE DATABASE ahhh_data;
+USE ahhh_data;
+CREATE TABLE questions(
+  QuestionID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  string VARCHAR(2000) NOT NULL,
+  upvotes INT NOT NULL,
+  posted_time TIMESTAMP,
+  namespace VARCHAR(200) NOT NULL,
+  answered TINYINT(1)
+);
+
+# Setup the database of admins
+CREATE DATABASE ahhh_admins;
+USE ahhh_admins;
+CREATE TABLE admins(
+  ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  admin_number INT UNSIGNED NOT NULL,
+  namespace VARCHAR(200) NOT NULL
+);
